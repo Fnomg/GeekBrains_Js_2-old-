50e103865ab1dd0d97e02b3e45ea429c35865416
Anna <annie_z@bk.ru>
2021-08-03 21:20:24 +0300

    сделала только часть задания

--- src/js/script.js
+++ src/js/script.js
@@ -22,6 +22,29 @@
 
 'use strict';
 
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// do not use fetch!! Only Promise! //Done
+let getRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject('Error')
+        } else {
+          resolve(xhr.responseText)
+        }
+      }
+    }
+    xhr.send();
+  })
+}
+
+// как сложнааа. Вторую часть так и не поняла как делать. Надеюсь я не одна такая.
+
+
 class ProductItem {
   constructor(product, img='https://via.placeholder.com/200x150') {
 
